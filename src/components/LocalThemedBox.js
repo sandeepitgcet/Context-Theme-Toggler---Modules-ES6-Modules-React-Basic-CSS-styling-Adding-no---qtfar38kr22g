a7185c91680c057a8e4a5b9a727737fac37dae6e
Sandeep Singh <33353409+sandeepitgcet@users.noreply.github.com>
2023-01-08 14:31:29 +0000

    updated

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -16,7 +16,7 @@ const LocalThemedBox = () => {
     return(
         <div className={"bg-"+localTheme} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
             <p className={"txt-"+localTheme} id="local-themed-text-container">THis is my Text</p>
-            <button className={"btn btn-"+localTheme} id="local-themed-text-container" onClick={changeLocalTheme}>Toggle local theme to {localTheme==='dark'?'light':'dark'}</button>
+            <button className={"btn btn-"+localTheme} id="local-theme-toggler" onClick={changeLocalTheme}>Toggle local theme to {localTheme==='dark'?'light':'dark'}</button>
         </div>
     )
 }
