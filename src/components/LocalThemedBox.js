f89abdb6ee000988c60094619160bc6ea97d8c22
Sandeep Singh <33353409+sandeepitgcet@users.noreply.github.com>
2023-01-08 14:26:48 +0000

    updated

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -3,22 +3,22 @@ import { ThemeContext } from './ThemeProvider';
 
 
 const LocalThemedBox = () => {
-    const theme = useContext(ThemeContext)
-const [localTheme,setLocalTheme] = useState(theme);
+    const [theme, ss] = useContext(ThemeContext)
+    const [localTheme,setLocalTheme] = useState(theme);
 
-useEffect(()=>{
-    setLocalTheme(theme)
-},[theme])
+    useEffect(()=>{
+        setLocalTheme(theme)
+    },[theme])
 
-const changeLocalTheme = () => {
-    setLocalTheme(localTheme=='dark'?"light":"dark")
-}
-return(
-    <div  className={"bg-"+localTheme} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
-        <p className={"txt-"+localTheme} id="local-themed-text-container">THis is my Text</p>
-        <button className={"btn btn-"+localTheme} id="local-themed-text-container" onClick={changeLocalTheme}>Toggle local theme to {localTheme==='dark'?'light':'dark'}</button>
-    </div>
-)
+    const changeLocalTheme = () => {
+        setLocalTheme(localTheme=='dark'?"light":"dark")
+    }
+    return(
+        <div className={"bg-"+localTheme} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
+            <p className={"txt-"+localTheme} id="local-themed-text-container">THis is my Text</p>
+            <button className={"btn btn-"+localTheme} id="local-themed-text-container" onClick={changeLocalTheme}>Toggle local theme to {localTheme==='dark'?'light':'dark'}</button>
+        </div>
+    )
 }
 
 export { LocalThemedBox }
